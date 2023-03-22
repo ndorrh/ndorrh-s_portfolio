ac3386cc901bbc9a347ef4ec1f4936f09b7079bf
ndorrh <ndorrh@gmail.com>
2023-03-22 16:23:03 +0100

    Add Social link component

--- src/common/SocialIcons.js
+++ src/common/SocialIcons.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Nav } from 'react-bootstrap'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
+import './socialIcons.css'
 
 const SocialIcons = () => {
   return (
-    <div className='social-icons'>
-      <a href='' ><img src={navIcon1} alt=''/></a>
-      <a href='' ><img src={navIcon2} alt='' /></a>
-      <a href='' ><img src={navIcon3} alt='' /></a>
-      <a href='' ><img src={''} alt='' /></a>
-      <a href='' ><img src={''} alt='' /></a>
-    </div>
+    <Nav className='social-icons'>
+      <Nav.Link href="" ><img src={navIcon1} alt='' /></Nav.Link >
+      <Nav.Link href="" ><img src={navIcon2} alt='' /></Nav.Link >
+      <Nav.Link href="" ><img src={navIcon3} alt='' /></Nav.Link >
+      <Nav.Link href="" ><img src={navIcon1} alt='' /></Nav.Link >
+      <Nav.Link href="" ><img src={navIcon3} alt='' /></Nav.Link >
+    </Nav>
   )
 }
 
