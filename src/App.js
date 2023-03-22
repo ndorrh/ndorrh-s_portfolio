b5c0524224df7008ddfdf31fb6143afad8485798
ndorrh <ndorrh@gmail.com>
2023-03-22 16:21:43 +0100

    import bootstrap css, navbar and banner component to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './common/NavBar'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './common/NavBar';
+import Banner from './features/banner/Banner';
 
 function App() {
   return (
     <div className="App">
-     <NavBar /> 
+     <NavBar />
+     <Banner /> 
     </div>
   );
 }
