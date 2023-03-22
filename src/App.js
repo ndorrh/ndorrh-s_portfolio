cd078b7f93195052112699f9f8b3292ddcb4a439
ndorrh <ndorrh@gmail.com>
2023-03-22 19:36:12 +0100

    Import skill style sheet and TabMenu

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './common/NavBar';
 import Banner from './features/banner/Banner';
+import './features/Tab/skill.css'
+import TabMenu from './features/Tab/Tab';
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
-     <Banner /> 
+      <NavBar />
+      <Banner />
+      <TabMenu />
     </div>
   );
 }
